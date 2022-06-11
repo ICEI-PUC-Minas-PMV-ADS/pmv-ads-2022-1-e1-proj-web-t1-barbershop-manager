bc327f3ea432194559b15d9106ddd7fbcd69c710
Vinicius-S-P <vinicius.sponciano@dtidigital.com.br>
2022-06-11 17:57:36 -0300

    feat: fluxo agendamento completo

--- src/scripts/agendamento.js
+++ src/scripts/agendamento.js
@@ -1,23 +1,50 @@
 var barbeiroDiv = document.getElementById("barbeiros-info");
 var inputHorario = document.getElementById("pesquisaHorario");
 var inputBarbeiro = document.getElementById("pesquisaBarbeiro");
+var sectionBarbeiros = document.getElementById("section-barbeiros");
+var sectionAgendamento = document.getElementById("section-agendamento");
+var buttonCancelar = document.getElementById("button-cancelar");
+var buttonFinalizar = document.getElementById("button-finalizar");
 var agendamentosDisponiveis = new XMLHttpRequest();
+var servicosPrestados = new XMLHttpRequest();
+
+var dadosParaEnvio = {
+  Servicos: [],
+  Total: 0,
+  Pagamento: "",
+  Horario: "",
+  Observacoes: "",
+};
 
 agendamentosDisponiveis.open("GET", "../../seeds/barber.json");
+servicosPrestados.open("GET", "../../seeds/service.json");
+servicosPrestados.send();
+
 agendamentosDisponiveis.onload = function () {
-  var barbeirosJson = JSON.parse(agendamentosDisponiveis.responseText);
+  sectionAgendamento.classList.add("hidden");
+  let barbeirosJson = JSON.parse(agendamentosDisponiveis.responseText);
   renderHTML(barbeirosJson);
+  addButtonEventListeners(barbeirosJson);
 };
+
 agendamentosDisponiveis.send();
 
 inputHorario.addEventListener("input", handlePesquisa, false);
 inputBarbeiro.addEventListener("input", handlePesquisa, false);
+buttonCancelar.addEventListener("click", handleClickCancelarAgendamento, false);
+buttonFinalizar.addEventListener(
+  "click",
+  handleClickFinalizarAgendamento,
+  false
+);
 
 function renderHTML(data) {
-  var htmlString = "";
+  let htmlString = "";
   for (let barbeiro of data) {
     htmlString +=
-      '<div class="blocoBarbeiro">' +
+      '<div id="' +
+      barbeiro.Id +
+      '"class="blocoBarbeiro">' +
       obtemHorarios(barbeiro.Horarios) +
       obtemImagen(barbeiro.Imagem, barbeiro.Nome) +
       obtemInfoServico(barbeiro.Servicos, barbeiro.Unidade) +
@@ -27,7 +54,7 @@ function renderHTML(data) {
 }
 
 function obtemHorarios(horarios) {
-  var stringHorarios =
+  let stringHorarios =
     '<div class="info">' + '<p class="textoInfo">Horarios Disponíveis</p>';
   for (let horario of horarios) {
     stringHorarios +=
@@ -50,7 +77,7 @@ function obtemImagen(url, nome) {
 }
 
 function obtemInfoServico(servicos, unidade) {
-  var stringInfoServico =
+  let stringInfoServico =
     '<div class="info">' +
     '<p class="textoInfo">Serviços Prestados</p>' +
     "<ul>";
@@ -63,7 +90,7 @@ function obtemInfoServico(servicos, unidade) {
 }
 
 function handlePesquisa() {
-  var barbeirosJson = JSON.parse(agendamentosDisponiveis.responseText);
+  let barbeirosJson = JSON.parse(agendamentosDisponiveis.responseText);
   if (barbeirosJson.length && barbeirosJson.length > 0) {
     if (inputBarbeiro.value.length && inputBarbeiro.value.length > 0) {
       barbeirosJson = barbeirosJson.filter(
@@ -75,13 +102,15 @@ function handlePesquisa() {
     }
     if (inputHorario.value.length && inputHorario.value.length > 0) {
       barbeirosJson = barbeirosJson.filter((barbeiro) =>
-        barbeiro.Horarios.some(horario => horario.includes(inputHorario.value))
+        barbeiro.Horarios.some((horario) =>
+          horario.includes(inputHorario.value)
+        )
       );
     }
   }
   barbeiroDiv.innerHTML = "";
   renderHTML(barbeirosJson);
-  agendamentosDisponiveis.send();
+  addButtonEventListeners(barbeirosJson);
 }
 
 function normalizarTexto(text) {
@@ -89,4 +118,120 @@ function normalizarTexto(text) {
     .normalize("NFD")
     .replace(/[\u0300-\u036f]/g, "")
     .toLowerCase();
+}
+
+function handleAgendamento(horario, servicos) {
+  sectionBarbeiros.classList.add("hidden");
+
+  dadosParaEnvio.Horario = horario;
+
+  let htmlString = "";
+  let servicosPrestadosJson = JSON.parse(servicosPrestados.responseText);
+  for (let servico of servicos) {
+    let servicoJson = servicosPrestadosJson.find(
+      (object) => object.Nome === servico
+    );
+    htmlString +=
+      "<div><img class='imagem-servico' id='" +
+      servicoJson.Nome +
+      "'src=" +
+      servicoJson.Imagem +
+      "></img><p>" +
+      servicoJson.Nome +
+      "</p></div>";
+  }
+  document
+    .getElementById("botoes-servicos")
+    .insertAdjacentHTML("beforeend", htmlString);
+
+  for (let servico of servicos) {
+    document
+      .getElementById(servico)
+      .addEventListener(
+        "click",
+        () =>
+          handleClickServico(
+            servico,
+            servicos.length,
+            servicosPrestadosJson.find((object) => object.Nome === servico)
+              .Preco
+          ),
+        false
+      );
+  }
+
+  document.getElementById("contador-selecao").textContent =
+    "0/" + servicos.length;
+  document.getElementById("total-a-pagar").textContent = "R$ 0,00";
+  sectionAgendamento.classList.remove("hidden");
+}
+
+function handleClickServico(servico, total, preco) {
+  if (!dadosParaEnvio.Servicos.find((objeto) => objeto === servico)) {
+    dadosParaEnvio.Servicos.push(servico);
+    console.log(preco.toFixed(2));
+    dadosParaEnvio.Total += preco;
+    console.log(dadosParaEnvio.Total);
+    let checkmarkString =
+      "<img id='checkmark-" +
+      servico +
+      "' class='checkmark' src='../../assets/icons/overlay-servico.png'></img>";
+    document
+      .getElementById(servico)
+      .insertAdjacentHTML("beforebegin", checkmarkString);
+    document.getElementById("contador-selecao").textContent =
+      dadosParaEnvio.Servicos.length + "/" + total;
+
+    document.getElementById("total-a-pagar").textContent =
+      "R$" + dadosParaEnvio.Total.toFixed(2);
+
+    document.getElementById("checkmark-" + servico).addEventListener(
+      "click",
+      () => {
+        dadosParaEnvio.Servicos.splice(
+          dadosParaEnvio.Servicos.indexOf(servico)
+        );
+        dadosParaEnvio.Total -= preco;
+        document.getElementById("checkmark-" + servico).remove();
+        document.getElementById("contador-selecao").textContent =
+          dadosParaEnvio.Servicos.length + "/" + total;
+        document.getElementById("total-a-pagar").textContent =
+          "R$ " + dadosParaEnvio.Total.toFixed(2);
+      },
+      false
+    );
+  } else {
+    dadosParaEnvio.Servicos.splice(dadosParaEnvio.Servicos.indexOf(servico));
+    document.getElementById("checkmark-" + servico).remove();
+  }
+}
+
+function handleClickCancelarAgendamento() {
+  sectionAgendamento.classList.add("hidden");
+  document.getElementById("botoes-servicos").innerHTML = "";
+  dadosParaEnvio = { Servicos: [], Total: 0, Pagamento: "", Horario: "" };
+  sectionBarbeiros.classList.remove("hidden");
+}
+
+function handleClickFinalizarAgendamento() {
+  sectionAgendamento.classList.add("hidden");
+  document.getElementById("botoes-servicos").innerHTML = "";
+  dadosParaEnvio.Observacoes = document.getElementById("texto-observacoes").value
+  dadosParaEnvio.Pagamento = document.getElementById("select-pagamento").value
+  console.log(dadosParaEnvio)
+  sectionBarbeiros.classList.remove("hidden");
+}
+
+function addButtonEventListeners(barbeiros) {
+  for (let barbeiro of barbeiros) {
+    const wrapper = document.getElementById(barbeiro.Id);
+
+    wrapper.addEventListener("click", (event) => {
+      const isButton = event.target.nodeName === "BUTTON";
+      if (!isButton) {
+        return;
+      }
+      handleAgendamento(event.target.textContent, barbeiro.Servicos);
+    });
+  }
 }
